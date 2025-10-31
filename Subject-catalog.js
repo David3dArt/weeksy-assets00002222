6141eec2d463ffcf2538ef62bd90f5d8be7dd35e
DgXx25 <122464702+David3dArt@users.noreply.github.com>
2025-10-31 22:40:11 +0400

    Add files via upload

---
+++ Subject-catalog.js
@@ -0,0 +1,4 @@
+[
+  { "id":"econ101", "name":"Economics I", "url":"https://raw.githubusercontent.com/you/repo/main/subjects/econ101.js" },
+  { "id":"math101", "name":"Mathematics I", "url":"https://raw.githubusercontent.com/you/repo/main/subjects/math101.js" }
+]
